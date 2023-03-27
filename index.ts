2e751b94404614f7f858bf814b776e0fe2db045c
Hans Inge Heien <hanshe@elkjop.no>
2023-03-27 18:45:02 +0200

    Added logic to demonstrate "One or more errors occurred" issue when not stopping node process

--- index.ts
+++ index.ts
@@ -1,11 +1,63 @@
 import * as express from "express";
-import * as pulumi from "@pulumi/pulumi";
+import { LocalWorkspace } from "@pulumi/pulumi/automation";
+
+interface ProgramResult {
+    summary: {
+        endTime: Date;
+    }
+};
 
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const createStackNumberingFunction = (): (() => number) => {
+    let i = 1;
+    return () => {
+        if (i == 3) {
+            i = 0;
+        }
+        return i++;
+    };
+}
+const getStackNumber = createStackNumberingFunction();
+
+const doThePulumiRun = async (alwaysSucceed: boolean): Promise<ProgramResult> => {
+    const stackNumber = getStackNumber();
+
+    const stack = await LocalWorkspace.createOrSelectStack({
+        stackName: `pulumiautomationbugspike${stackNumber}`,
+        projectName: "pulumiautomationbugspike",
+        program: async () => {
+            if (!alwaysSucceed && stackNumber % 3 == 0) {
+                throw new Error("Failing this run");
+            }
+        },
+    });
+
+    try {
+        return await stack.up({ onOutput: console.info });
+    } catch (error) {
+        console.log("Catching the error so that the node process survives");
+        return {
+            summary: {
+                endTime: new Date(),
+            },
+        }
+    }
+};
+
+app.get("/faileverythreetimes", async (req, res) => {
+    const programResult = await doThePulumiRun(false);    
+    res.send({
+        programEndDate: programResult.summary.endTime,
+    });
+});
+
+app.get("/succeed", async (req, res) => {
+    const programResult = await doThePulumiRun(true);    
+    res.send({
+        programEndDate: programResult.summary.endTime,
+    });
 });
 
 app.listen(port, () => {
